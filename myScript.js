2a220f2995c98c3cdb59a7494943452dce547b15
daliduraku <mesherbetmolla@gmail.com>
2024-09-26 23:30:04 -0400

    Created the function getComputerChoice, uses Math.random to get an answer of rock, paper or scissors

--- myScript.js
+++ myScript.js
@@ -1,4 +1,13 @@
 
 function getComputerChoice(){
-    
+    let num = Math.random();
+
+    if(num >=0 && num < 0.333){
+        return ("rock")
+    }else if(num >= 0.333 && num < 0.666){
+        return("paper")
+    }else{
+        return("scissors")
+    }
 }
+
